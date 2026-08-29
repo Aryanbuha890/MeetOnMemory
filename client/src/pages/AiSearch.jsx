86725e0ae21df75697a8d342801745464261e8de
Aryanbuha890 <aryan.buha89@gmail.com>
2026-08-29 19:30:07 +0530

    style: format AiSearch.jsx with Prettier

--- client/src/pages/AiSearch.jsx
+++ client/src/pages/AiSearch.jsx
@@ -447,8 +447,6 @@ const AiSearch = () => {
     window.open(`/meeting/${meetingId}?t=${seconds}`, "_blank");
   };
 
-
-
   return (
     <div className="min-h-screen bg-gradient-to-br from-gray-50 to-blue-50 dark:from-gray-900 dark:to-gray-800 flex flex-col">
       <Navbar />
